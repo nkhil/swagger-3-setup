d2ce57a73f69599707ed176e66e87d396bb8e010
Nikhil <nikhil@jigsaw.xyz>
2020-06-08 13:07:54 +0100

    :sparkles: add OpenApiValidator

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path = require('path');
+const { OpenApiValidator } = require('express-openapi-validator');
+const config = require('./config');
+
+const app = express();
+const apiSpec = path.join(__dirname, `../definitions/${config.name}.yml`);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+new OpenApiValidator({
+  apiSpec,
+  validateResponses: true,
+  operationHandlers: path.join(__dirname, './handlers'),
+})
+  .install(app)
+  .then(() => {
+    app.use((err, _, res) => {
+      res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+      });
+    });
+  });
 
 module.exports = app;
